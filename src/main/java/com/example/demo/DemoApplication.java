5b1937d3f517b66cd85845757f3b6587c8b05eb2
Andy Wilkinson <wilkinsona@vmware.com>
2023-05-24 12:04:00 +0100

    Remove Spring Boot and Spring Data JPA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,55 +1,102 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.EnumSet;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.EnumSet;
+import com.zaxxer.hikari.HikariDataSource;
 
-@SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-    private final UserRepository userRepository;
-    private final PlatformTransactionManager transactionManager;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
-    public DemoApplication(UserRepository userRepository, PlatformTransactionManager transactionManager) {
-        this.userRepository = userRepository;
-        this.transactionManager = transactionManager;
-    }
+public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    @Override
-    public void run(String... args) {
-        createUsers();
-        deleteUsers();
-    }
+	private final PlatformTransactionManager transactionManager;
 
-    private void deleteUsers() {
-        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-        transactionTemplate.execute((status) -> {
-            userRepository.deleteAll();
-            return null;
-        });
-    }
+	public DemoApplication(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
+	}
 
-    private void createUsers() {
-        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-        transactionTemplate.execute((status) -> {
-            User a = new User();
-            a.setUserRoles(EnumSet.of(UserRole.USER));
-            a = userRepository.save(a);
+	public static void main(String[] args) {
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
+			context.register(AppConfiguration.class, DemoApplication.class);
+			context.refresh();
+			DemoApplication demo = context.getBean(DemoApplication.class);
+			demo.createUsers();
+			demo.deleteUsers();
+		}
+	}
 
-            User user = new User();
-            user.setUserRoles(EnumSet.of(UserRole.USER));
-            user.setCreatedBy(a);
-            user = userRepository.save(user);
-            System.out.println(user.getCreatedBy().getId());
-            return null;
-        });
-    }
+	private void deleteUsers() {
+		TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+		transactionTemplate.execute((status) -> {
+			for (User user : this.entityManager.createQuery("Select u from User u", User.class).getResultList()) {
+				this.entityManager.remove(user);
+			}
+			return null;
+		});
+	}
+
+	private void createUsers() {
+		TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+		transactionTemplate.execute((status) -> {
+			User a = new User();
+			a.setUserRoles(EnumSet.of(UserRole.USER));
+			this.entityManager.persist(a);
+			User user = new User();
+			user.setUserRoles(EnumSet.of(UserRole.USER));
+			user.setCreatedBy(a);
+			this.entityManager.persist(user);
+			System.out.println(user.getCreatedBy().getId());
+			return null;
+		});
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	static class AppConfiguration {
+
+		@Bean
+		DataSource dataSource() {
+			HikariDataSource dataSource = new HikariDataSource();
+			dataSource.setDriverClassName("org.h2.Driver");
+			dataSource.setJdbcUrl("jdbc:h2:mem:test");
+			return dataSource;
+		}
+
+		@Bean
+		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(DataSource dataSource) {
+			LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+			factory.setDataSource(dataSource);
+			factory.setJpaDialect(new HibernateJpaDialect());
+			factory.setPackagesToScan("com.example.demo");
+			factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+			factory.setJpaPropertyMap(Map.of(
+					AvailableSettings.HBM2DDL_AUTO, "update", 
+					AvailableSettings.SHOW_SQL, "true"
+			));
+			return factory;
+		}
+
+		@Bean
+		PlatformTransactionManager transactionManager() {
+			return new JpaTransactionManager();
+		}
+
+	}
+
 }
--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,9 +1,18 @@
 package com.example.demo;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-
 import java.util.Set;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "application_user")
